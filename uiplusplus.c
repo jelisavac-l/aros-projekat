96c354f1c6082e2bb220def568b360e41b3e7305
jelisavac-l <jelisavacluka63@gmail.com>
2024-04-08 23:12:15 +0200

    Cisto da se zeleni na githubu

--- uiplusplus.c
+++ uiplusplus.c
@@ -30,13 +30,18 @@ extern void print_help(int proc_type)
 {
     if(proc_type == SRV)
     {
-        print_splash_screen(proc_type);
+        // print_splash_screen(proc_type);
         printf("Usage: ./server path_to_file\n");
+        printf("Options:\n");
+        printf("\t -l\tListen for requests on message queue. Work in progress.\n");
     }
     else if(proc_type == CLI)
     {
-        print_splash_screen(proc_type);
+        // print_splash_screen(proc_type);
         printc("You are seriously asking for help on a client application, you little bitch?\n"
                 "It still does nothing. Sometimes it accepts some messages. Usually it doesn't.\n", RED);
+
+        printf("Options:\n");
+        printf("\t -r\tSend request to a server. Work in progress.\n");
     }
 }
